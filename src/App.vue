27742bac736670e56e139f5ed401b461a466b0b6
konaraddio <omkar.konaraddi@gmail.com>
2018-03-11 18:01:59 -0400

    Using button and quote components, add bulma, add ibm plex font, add styling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <p>{{month}} / {{day}}</p>
-    <h3>{{quote}}</h3>
-    <button v-on:click="prevQuote">back</button>
-    <button v-on:click="nextQuote">forward</button>
+    <p class="subtitle is-5">{{year}}.{{String(month).padStart(2, '0')}}.{{String(day).padStart(2, '0')}}</p>
+    <QuoteText >{{quote}}</QuoteText> <br>
+    <section id="buttons">
+      <BulmaButton :onClick="prevQuote">
+        <span class="icon">
+          <i class="fas fa-angle-left"></i>
+        </span>
+        <span>Prev</span>
+      </BulmaButton>
+      <BulmaButton :onClick="nextQuote">
+        <span>Next</span>
+        <span class="icon">
+          <i class="fas fa-angle-right"></i>
+        </span>
+      </BulmaButton>
+    </section>
   </div>
 </template>
 
 <script>
+import QuoteText from './components/QuoteText'
+import BulmaButton from './components/BulmaButton'
+
 import quotes from './quotes'
 
 export default {
   name: 'App',
   components: {
-    
+    QuoteText,
+    BulmaButton
   },
   data() {
     return {
@@ -24,6 +40,9 @@ export default {
   computed: {
     quote() {
       return quotes[this.month - 1][this.day - 1]
+    },
+    year(){
+      return (new Date()).getFullYear()
     }
   },
   methods: {
@@ -59,3 +78,50 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import "~bulma/sass/utilities/initial-variables";
+$yellow: #F8EFBA;
+$orange: #FEA47F;
+$blue: #25CCF7;
+$dark: #2C3A47;
+$primary: $dark;
+
+$family-primary: 'IBM Plex Mono';
+@import "~bulma/sass/utilities/_all.sass";
+@import "~bulma/sass/base/_all.sass";
+/*import elements, grid, components, and then layout (in that order)*/
+@import "~bulma/sass/elements/box.sass";
+@import "~bulma/sass/elements/button.sass";
+@import "~bulma/sass/elements/content.sass";
+@import "~bulma/sass/elements/image.sass";
+@import "~bulma/sass/elements/icon.sass";
+@import "~bulma/sass/elements/other.sass";
+@import "~bulma/sass/elements/tag.sass";
+@import "~bulma/sass/elements/title.sass";
+@import "~bulma/sass/grid/columns.sass";
+@import "~bulma/sass/components/navbar.sass";
+@import "~bulma/sass/components/tabs.sass";
+@import "~bulma/sass/components/menu.sass";
+@import "~bulma/sass/layout/_all.sass";
+
+html{
+  background: $yellow;
+}
+
+#app{
+  text-align: center;
+  margin-top: 128px;
+}
+
+#buttons{
+  position: fixed;
+  bottom: 128px;
+  @media screen and (max-width: 768px){
+    bottom: 64px;
+  }
+  left: 0;
+  right: 0;
+}
+
+</style>
---
+++ src/components/BulmaButton.vue
@@ -0,0 +1,16 @@
+<template>
+<button @click="onClick" class="button is-rounded is-primary is-outlined">
+  <slot></slot>
+</button>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
